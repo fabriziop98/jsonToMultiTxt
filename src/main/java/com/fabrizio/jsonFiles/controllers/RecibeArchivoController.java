e273dd1fc29a7abc99527041d36d643041b84975
fabriziop98 <fabriziop@live.com.ar>
2020-07-20 15:28:22 -0300

    cambios detalles y manejo de errores

--- src/main/java/com/fabrizio/jsonFiles/controllers/RecibeArchivoController.java
+++ src/main/java/com/fabrizio/jsonFiles/controllers/RecibeArchivoController.java
@@ -1,12 +1,5 @@
 package com.fabrizio.jsonFiles.controllers;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import org.json.CDL;
-import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.fabrizio.jsonFiles.service.DivideArchivoService;
-import com.fabrizio.jsonFiles.util.FileUtil;
 
 @Controller
 @RequestMapping(value="/")
@@ -25,148 +17,23 @@ public class RecibeArchivoController {
 	@Autowired
 	private DivideArchivoService archivoService;
 	
-//	@PostMapping("/enviar")
-//	public String inicio(@RequestParam(required = false, value = "file") MultipartFile archivo){
-//		archivoService.readFile(archivo);
-//		return "success";
-//	}
-
 	@GetMapping("")
 	public String inicio() {
 		return "inicio";
 	}
 	
-	public File multipartToFile(MultipartFile file){
-		File x = null;
-		File directorio = new File(FileUtil.RUTA_ARCHIVOS);
-        if (!directorio.exists()) {
-            directorio.mkdir();
-        }
-        String archivo = file.getOriginalFilename();
-        x = new File(FileUtil.RUTA_ARCHIVOS + archivo);
-        int fileNo = 0;
-        while (x.exists()) {
-            fileNo++;
-            int punto = archivo.lastIndexOf('.');
-            String extension = archivo.substring(punto);
-            String nombre = archivo.substring(0, punto);
-            archivo = nombre + "(" + fileNo + ")" + extension;
-            x = new File(FileUtil.RUTA_ARCHIVOS + archivo);
-        }
-        try {
-			file.transferTo(x);
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	    return x;
-	}
-	
 	@PostMapping("/cargar")
 	public String cargar(@RequestParam(required = false, name = "excel") MultipartFile file,RedirectAttributes flash) {
-		InputStream is;
 		try {
-			is = file.getInputStream();
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-		String inputStr, csv = "";
-
-		while ((inputStr = br.readLine()) != null){
-		    csv += inputStr +"\n";
-		}
-
-		JSONArray array = CDL.toJSONArray(csv);
-		archivoService.readFile(array);
-		
-		} catch (IOException e) {
+			archivoService.readFile(file);
+		} catch (Exception e) {
+			flash.addFlashAttribute("error", "Ocurrió un error en la conversión del archivo.");
 			e.printStackTrace();
+			return "redirect:/";
 		}
 		flash.addFlashAttribute("success", "Archivos generados con exito");
 		return "redirect:/";
 
 	}
-	
-//	@PostMapping("/cargar")
-//    public String cargaMasiva(@RequestParam(required = false, name = "excel") MultipartFile file, RedirectAttributes flash) {
-//
-//		File x = multipartToFile(file);
-//		String line = "";
-//        String cvsSplitBy = ",";
-//        JSONArray jsonArray = new JSONArray();
-//        List<ArchivoJson>listaParaGuardar = new ArrayList<>();
-//        try (BufferedReader br = new BufferedReader(new FileReader(x))) {
-//        	
-//            while ((line = br.readLine()) != null) {
-//            	ArchivoJson archivoJson = new ArchivoJson();
-//                String[] cadena = line.split(cvsSplitBy);
-//                
-//               	archivoJson.setNumber(cadena[0]);
-//               	archivoJson.setStatus(cadena[1]);
-//               	archivoJson.setCreatedDate(cadena[2]);
-//               	archivoJson.setDueDate(cadena[3]);
-//               	archivoJson.setTotal(cadena[4]);
-//               	archivoJson.setTaxes(cadena[5]);
-//               	archivoJson.setDiscount(cadena[6]);
-//               	archivoJson.setAmountPaid(cadena[7]);
-//               	archivoJson.setAmountDue(cadena[8]);
-//               	archivoJson.setOrganizationName(cadena[9]);
-//               	archivoJson.setClientName(cadena[10]);
-//               	archivoJson.setOrganizationAddress(cadena[11]);
-//               	archivoJson.setClientAddress(cadena[12]);				
-//               	
-//               	listaParaGuardar.add(archivoJson);
-//            }
-//            for (ArchivoJson archivoJson : listaParaGuardar) {            	
-//            	Object number = (Object)archivoJson.getNumber();
-//            	Object Status = (Object)archivoJson.getStatus();
-//            	Object CreatedDate = (Object)archivoJson.getCreatedDate();
-//            	Object Total = (Object)archivoJson.getTotal();
-//            	Object Taxes = (Object)archivoJson.getTaxes();
-//            	Object Discount = (Object)archivoJson.getDiscount();
-//            	Object AmountPaid = (Object)archivoJson.getAmountPaid();
-//            	Object ClientName = (Object)archivoJson.getClientName();
-//            	Object OrganizationAddress = (Object)archivoJson.getOrganizationAddress();
-//            	Object ClientAddress = (Object)archivoJson.getClientAddress();
-//            	String propiedades [] = {"Number","Status","CreatedDate","Total","Taxes","Discount","AmountPaid","ClientName","OrganizationAddress","ClientAddress"};
-//            	Object valores [] = {number,Status,CreatedDate,Total,Taxes,Discount,AmountPaid,ClientName,OrganizationAddress,ClientAddress};
-//            	String prueba = getSimpleJSON(propiedades,valores);
-//            	System.out.println(prueba);
-//            	jsonArray.add(prueba);
-//            }
-//            jsonArray.remove(0);
-//            archivoService.readFile(jsonArray);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return "redirect:/";
-//    }
-	
-	
-	
-	  public static String getSimpleJSON(String propiedad, Object valor) {
-	        return "{\"" + propiedad + "\":\"" + valor + "\"}";
-	    }
-	  
-	  public static String getSimpleJSON(String[] propiedades, Object[] valores) {
-
-	        StringBuilder sb = new StringBuilder("{");
-	        for (int n = 0; n < propiedades.length; n++) {
-	            if (valores[n].getClass().isAssignableFrom(Number.class)
-	                    || valores[n].getClass().isAssignableFrom(Double.class)
-	                    || valores[n].getClass().isAssignableFrom(Integer.class)) {
-	                sb.append("\"").append(propiedades[n]).append("\":").append(valores[n]);
-	            } else {
-	                sb.append("\"").append(propiedades[n]).append("\":\"").append(valores[n]).append("\"");
-	            }
-
-	            if (n + 1 < propiedades.length) {
-	                sb.append(",");
-	            }
-	        }
-
-	        return sb.append("}").toString();
-	    }
 	
 }
--- src/main/java/com/fabrizio/jsonFiles/service/DivideArchivoService.java
+++ src/main/java/com/fabrizio/jsonFiles/service/DivideArchivoService.java
@@ -1,20 +1,16 @@
 package com.fabrizio.jsonFiles.service;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileOutputStream;
 import java.io.IOException;
-
-import java.util.regex.Pattern;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
+import org.json.CDL;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,133 +19,73 @@ import com.fabrizio.jsonFiles.util.FileUtil;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class DivideArchivoService {
 
-	private Logger LOGGER = LoggerFactory.getLogger(DivideArchivoService.class);
 
+	public void readFile(MultipartFile file) throws Exception{
+		
 
-	public void readFile(JSONArray objetos) {
-		JSONParser parser = new JSONParser();
 		ArchivoTxt txt = new ArchivoTxt();
 		try {
-//			Object obj = parser.parse(new FileReader(convertMultiPartFileToFile(archivo)));
-//			JSONArray companyList = (JSONArray) obj;
-			JSONArray companyList = objetos; 
+			JSONArray companyList = CsvtoJSON(file);
 			List<ArchivoTxt> listaTxt = new ArrayList<>();
-			
+
 			PrintWriter writer;
 			try {
-				for (Object o : companyList){
+				for (Object o : companyList) {
 					System.out.println(o.toString());
-					 JSONObject factura = (JSONObject) o;
-					writer = new PrintWriter(FileUtil.RUTA_ARCHIVOS + "factura_"+ (String) factura.get("Number") + ".txt", "UTF-8");
-				    writer.println("factura/tipcom=Factura");
-				    writer.println("factura/codven=ADM");
-				    String numero = (String) factura.get("Number");
-				    txt.setFactura(numero);
-				    writer.println("factura/codcta="+numero);
-				    writer.println("linea/codart=1");
-				    writer.println("linea/cantid=5");
-				    String precio = (String) factura.get("Total");
-				    txt.setPrecio(precio);
-				    writer.println("linea/precio="+precio);
-				    writer.println("linea/grabar");
+					JSONObject factura = (JSONObject) o;
+					writer = new PrintWriter(
+							FileUtil.RUTA_ARCHIVOS + "factura_" + (String) factura.get("Number") + ".txt", "UTF-8");
+					writer.println("factura/tipcom=Factura");
+					writer.println("factura/codven=ADM");
+					String numero = (String) factura.get("Number");
+					txt.setFactura(numero);
+					writer.println("factura/codcta=" + numero);
+					writer.println("linea/codart=1");
+					writer.println("linea/cantid=5");
+					String precio = (String) factura.get("Total");
+					txt.setPrecio(precio);
+					writer.println("linea/precio=" + precio);
+					writer.println("linea/grabar");
 
-				    listaTxt.add(txt);
-				    
-				    writer.close();
-				  }
-				
-				
-				
+					listaTxt.add(txt);
+
+					writer.close();
+				}
+
 			} catch (FileNotFoundException | UnsupportedEncodingException e) {
 				e.printStackTrace();
 			}
-			
-			
-			
-			
-			
-			
-			
-			
-//			String jSonString = companyList.toJSONString();
-//			String newString = jSonString.replace("Number", "factura/codcta=");
-//			System.out.println(newString);
-////			newString = newString.replace("\"Discount\":\"$ 0,00\",", "");
-////			System.out.println(newString);
-////			newString = newString.replace("\"Status\":\"Pago\"","factura/tipcom=Factura");
-////			System.out.println(newString);
-////			newString = newString.replace("\"Amount due\":\"$ 0,00\"","factura/codven=ADM");
-////			newString = newString.replace("\"Organization name\":\"NUEVAS REDES S.A\"","");
-//			
-//
-//			JSONArray jsonMod = (JSONArray) parser.parse(newString);
-//			Iterator<JSONObject> iteratorMod = jsonMod.iterator();
-//
-//			while (iteratorMod.hasNext()) {
-//				generadorArchivo(lineasTexto(iteratorMod.next()));
-//			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw e;
 		}
 
 	}
 
-	public File convertMultiPartFileToFile(final MultipartFile multipartFile) {
-		final File file = new File(multipartFile.getOriginalFilename());
-		try (final FileOutputStream outputStream = new FileOutputStream(file)) {
-			outputStream.write(multipartFile.getBytes());
-		} catch (final IOException ex) {
-			LOGGER.error("Error converting the multi-part file to file= ", ex.getMessage());
-		}
-		return file;
-	}
-
-
-	public void generadorArchivo(String[] lineasTexto) {
-
-		PrintWriter writer;
+	public JSONArray CsvtoJSON(MultipartFile file) throws Exception{
+		InputStream is;
 		try {
-			writer = new PrintWriter(FileUtil.RUTA_ARCHIVOS + UUID.randomUUID() + ".txt", "UTF-8");
-			for (String s : lineasTexto) {
-				writer.println(s);
+			is = file.getInputStream();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			String inputStr, csv = "";
+
+			while ((inputStr = br.readLine()) != null) {
+				csv += inputStr + "\n";
 			}
-			writer.close();
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+
+			JSONArray array = CDL.toJSONArray(csv);
+			return array;
+		} catch (IOException e) {
 			e.printStackTrace();
+			throw e;
 		}
-
 	}
-
-	public String[] lineasTexto(JSONObject iteratorModificado) {
-
-		String objetoString = iteratorModificado.toString();
-		objetoString = objetoString.substring(1, objetoString.length() - 1);
-
-		String separador = Pattern.quote(",");
-
-		String[] parts = objetoString.split(separador);
-
-		return parts;
-	}
-	
-//	private void convertidor(JSONArray companyList) {
-//		ArrayList<String> listdata = new ArrayList<String>();     
-//		JSONArray jArray = companyList; 
-//		if (jArray != null) { 
-//		   for (int i=0;i<jArray.length();i++){ 
-//		    listdata.add(jArray.getString(i));
-//		   } 
-//		}
-//		for(String i : listdata) {
-//			System.out.println(i);
-//		}
-//	}
 	
 
 }
